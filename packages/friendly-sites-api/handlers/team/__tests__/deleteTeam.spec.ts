a9fae1cfe27431cc69b5ff4fe73f7fa60e86dcf1
Anthony Collins <a.r.c_@hotmail.co.uk>
2024-04-08 21:42:02 +0000

    delete method on teams

---
+++ packages/friendly-sites-api/handlers/team/__tests__/deleteTeam.spec.ts
@@ -0,0 +1,49 @@
+import { describe, test, expect, beforeAll } from "bun:test"
+import { entity as teamEntity } from "../../../entities/team"
+import deleteTeam from "../deleteTeam"
+import type { APIGatewayProxyEvent } from "aws-lambda"
+
+describe('deleteTeam', () => {
+
+    let team
+    const teamName = 'Team ' + crypto.randomUUID()
+    const authUser = {
+        sub: crypto.randomUUID()
+    }
+
+    beforeAll(async () => {
+        const teamRes = await teamEntity.create({
+            name: teamName,
+            users: [authUser.sub]
+        }).go()
+
+        team = teamRes.data
+    })
+
+    test('removes team record from db', async () => {
+        // @ts-ignore
+        const mockRequest = {
+            httpMethod: "DELETE",
+            headers: {},
+            requestContext: {
+                authorizer: {
+                    claims: {
+                        sub: authUser.sub
+                    }
+                }
+            },
+            pathParameters: {
+                id: team.id
+            }
+        } as APIGatewayProxyEvent
+
+        const req = await deleteTeam(mockRequest)
+
+        expect(req.statusCode).toBe(200)
+        // should return full record that has been deleted
+        expect(JSON.parse(req.body)).toHaveProperty('id')
+        expect(JSON.parse(req.body)).toHaveProperty('name')
+        // expect no record to be returned
+        expect(await teamEntity.get({ id: team.id }).go()).toMatchObject({ data: null })
+    })
+})
