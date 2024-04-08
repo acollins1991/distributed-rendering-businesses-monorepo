e531027b004451cdb8ea020f6dcbeb389d531beb
Anthony Collins <a.r.c_@hotmail.co.uk>
2024-04-08 21:45:05 +0000

    read method

---
+++ packages/friendly-sites-api/handlers/team/__tests__/readTeam.spec.ts
@@ -0,0 +1,45 @@
+import { describe, test, expect, beforeAll } from "bun:test"
+import { entity as teamEntity } from "../../../entities/team"
+import readTeam from "../readTeam"
+import type { APIGatewayProxyEvent } from "aws-lambda"
+
+describe('readTeam', () => {
+
+    let team
+    const teamName = 'Team ' + crypto.randomUUID()
+    const authUser = {
+        sub: crypto.randomUUID()
+    }
+
+    beforeAll(async () => {
+        const teamRes = await teamEntity.create({
+            name: teamName,
+            users: [authUser.sub]
+        }).go()
+
+        team = teamRes.data
+    })
+
+    test('returns same team record with updated name', async () => {
+        // @ts-ignore
+        const mockRequest: APIGatewayProxyEvent = {
+            httpMethod: "GET",
+            headers: {},
+            requestContext: {
+                authorizer: {
+                    claims: {
+                        sub: authUser.sub
+                    }
+                }
+            },
+            pathParameters: {
+                id: team.id
+            }
+        } as APIGatewayProxyEvent
+
+        const req = await readTeam(mockRequest)
+
+        expect(req.statusCode).toBe(200)
+        expect(JSON.parse(req.body).id).toBe(team.id)
+    })
+})
---
+++ packages/friendly-sites-api/handlers/team/readTeam.ts
@@ -0,0 +1,55 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { entity } from "../../entities/team"
+import type { ElectroError } from 'electrodb';
+import { getAuthUserFromRequestEvent } from '../../utils/getAuthUserFromRequestEvent';
+
+export default async function (request: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+
+    if (request.httpMethod !== 'GET') {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({
+                message: 'Bad Request'
+            })
+        };
+    }
+
+    if (!request.pathParameters || !request.pathParameters['id']) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({
+                message: 'Bad Request: missing team id'
+            })
+        };
+    }
+
+    const user = getAuthUserFromRequestEvent(request)
+
+    if (typeof user !== 'string') {
+        return {
+            statusCode: 403,
+            body: JSON.stringify({
+                message: 'Not authorised'
+            })
+        }
+    }
+
+    try {
+        const { id: teamId } = request.pathParameters;
+        const team = await entity.get({ id: teamId }).go()
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(team.data)
+        }
+    } catch (e) {
+        const error = e as ElectroError
+
+        console.log(error)
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error)
+        }
+    }
+}
