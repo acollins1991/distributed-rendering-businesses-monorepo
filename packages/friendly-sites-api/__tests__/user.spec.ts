2f6a0c11e271bb74d8597542a609dfa159350027
Anthony Collins <a.r.c_@hotmail.co.uk>
2024-05-15 10:28:30 +0000

    patch user endpoint added

--- packages/friendly-sites-api/__tests__/user.spec.ts
+++ packages/friendly-sites-api/__tests__/user.spec.ts
@@ -1,25 +1,24 @@
 import { describe, test, expect, beforeAll } from "bun:test"
-import { type User } from "../entities/user"
+import { entity, type User } from "../entities/user"
 import { entity as sessionEntity } from "../entities/sessions"
 import { faker } from "@faker-js/faker"
 import createUserFactory from "../factories/User"
-import { auth } from "../auth"
 import validateBearerToken from "../utils/validateBearerToken"
 import type { Session } from "lucia"
 import { app } from ".."
 
 describe("/user", () => {
 
+    let databaseUser: User
+    let bearerToken: Session["id"]
+
+    beforeAll(async () => {
+        const { user, session } = await createUserFactory()
+        databaseUser = user
+        bearerToken = session.id
+    })
+
     describe("GET", () => {
-
-        let databaseUser: User
-        let bearerToken: Session["id"]
-
-        beforeAll(async () => {
-            const { user, session } = await createUserFactory()
-            databaseUser = user
-            bearerToken = session.id
-        })
 
         test("valid bearer token returns user from session", async () => {
             // confirm session currently valid
@@ -43,6 +42,45 @@ describe("/user", () => {
             expect(res.status).toBe(200)
             const json = await res.json()
             expect(json.id).toBe(user?.id)
+        })
+
+    })
+
+    describe("PATCH", () => {
+
+        test('updates the user record based on the bearer token', async () => {
+
+            const newFirstName = faker.person.firstName()
+            const newEmail = faker.internet.email({
+                firstName: newFirstName,
+            })
+
+            //
+            const res = await app.request("/user", {
+                method: "PATCH",
+                headers: {
+                    "content-type": "application/json"
+                }
+            }, {
+                event: {
+                    headers: {
+                        authorization: `Bearer ${bearerToken}`
+                    },
+                    body: JSON.stringify({
+                        first_name: newFirstName,
+                        email: newEmail
+                    })
+                }
+            })
+
+            expect(res.status).toBe(200)
+            const json = await res.json()
+            expect(json.id).toBe(databaseUser.userId)
+
+            // check record has updated
+            const { data: refreshedUserRecord } = await entity.get({ userId: databaseUser.userId }).go()
+            expect(refreshedUserRecord?.first_name).toBe(newFirstName)
+            expect(refreshedUserRecord?.email).toBe(newEmail)
         })
 
     })
--- packages/friendly-sites-api/routes/user.ts
+++ packages/friendly-sites-api/routes/user.ts
@@ -2,6 +2,9 @@ import { Hono } from "hono";
 import type { LambdaBindings } from "../types";
 import apiValidateBearerTokenMiddleware from "../utils/apiValidateBearerTokenMiddleware";
 import type { User } from "lucia";
+import { entity } from "../entities/user";
+import { z } from "zod";
+import validateLambdaEvent from "../utils/validateLambdaEvent";
 
 const user = new Hono<{ Bindings: LambdaBindings }>()
 
@@ -12,6 +15,35 @@ user.get(
         try {
 
             const user = c.get("user") as User
+
+            return c.json(user, 200)
+
+        } catch (e) {
+            return c.json(e, 400)
+        }
+    })
+
+const PatchBodySchema = z.object({
+    first_name: z.string().optional(),
+    last_name: z.string().optional(),
+    email: z.string().optional()
+})
+type PatchBodySchemaType = z.infer<typeof PatchBodySchema>;
+user.patch(
+    "/",
+    validateLambdaEvent({
+        bodySchema: PatchBodySchema
+    }),
+    apiValidateBearerTokenMiddleware,
+    async (c) => {
+
+        const patchObject = JSON.parse(c.env.event.body as string) as PatchBodySchemaType
+
+        try {
+
+            const user = c.get("user") as User
+
+            const updatedUser = await entity.patch({ userId: user.userId }).set(patchObject).go()
 
             return c.json(user, 200)
 
